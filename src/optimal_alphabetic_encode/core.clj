f30c77c7a2d969ec7ebee8cd80cd352209834708
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-12-15 09:37:43 +0100

    Have implemented 2 functions. First return distinct array of chars. Second calculate count of array

--- src/optimal_alphabetic_encode/core.clj
+++ src/optimal_alphabetic_encode/core.clj
@@ -1,6 +1,14 @@
 (ns optimal-alphabetic-encode.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(set! *unchecked-math* :warn-on-boxed)
+(set! *warn-on-reflection* true)
+
+(def rec "MARIBOR")
+
+(defn string-to-array ^chars [^String rec]
+  (vec (distinct rec))
+  )
+
+(defn length-array ^long [^chars array]
+  (count array)
+  )
