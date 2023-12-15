4dba0bb3d8e15b547a1aec6d439b7e5a64f62d23
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-12-15 09:13:37 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to optimal-alphabetic-encode
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/optimal_alphabetic_encode/core_test.clj
@@ -0,0 +1,7 @@
+(ns optimal-alphabetic-encode.core-test
+  (:require [clojure.test :refer :all]
+            [optimal-alphabetic-encode.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
